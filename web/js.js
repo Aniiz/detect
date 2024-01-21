b1897732a474986f47585b9b86bf1bb75d4696c2
Anizio Moreira <anizmoreira@hotmail.com>
2024-01-21 13:39:56 -0300

    correcao requisicao para producao

--- web/js.js
+++ web/js.js
@@ -1,18 +1,31 @@
 const fileSelector = document.getElementById("file-selector");
 const send = document.getElementById("send");
 const myDiv = document.getElementById('my-div');
-const url = 'http://127.0.0.1:5000/';
+// const url = 'http://127.0.0.1:5000/';
 // const url = 'https://homo-srv-cicatribio-ia-rec.herokuapp.com/';
-// const url = 'https://recognition-app-fw7ua.ondigitalocean.app/'
-//request
+const url = 'https://recognition-app-fw7ua.ondigitalocean.app/'
+
+
 send.addEventListener('click', async (event) => {
     event.preventDefault();
+    myDiv.innerHTML = ``
     const img = fileSelector.files[0];
     const b64Image = await tob64(img);
     const resp = await imgPost(b64Image);
-    console.log(resp)
-    const imgSrc = 'data:image/jpeg;base64,' + resp.imgPred
-    myDiv.innerHTML = `<img class="img" src="${imgSrc}">`;
+
+    if (resp.imgPred) {
+        const imgSrc = 'data:image/jpeg;base64,' + resp.imgPred
+        myDiv.innerHTML = `<img class="img" src="${imgSrc}">`
+    }
+    else {
+
+        if (resp.resp) {
+            myDiv.innerHTML = `<p class="true"> Ferida Detectada </p>`
+        }
+        else
+            myDiv.innerHTML = `<p class="false"> Ferida não Detectada </p>`
+    }
+
 });
 
 //to B64
